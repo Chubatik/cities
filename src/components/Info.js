d86dbdd2d522ac6a6c0cfeb5142472bb123f7582
Chubatik <49166879+Chubatik@users.noreply.github.com>
2021-10-05 20:19:18 +0300

    Init commit

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>bla bla</h1>
+                <p>another bla</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
